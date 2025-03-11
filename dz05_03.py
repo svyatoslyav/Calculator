ed5b5549ba30a6013d9d8e035626120b2b6fb251
svyatoslyav <swatock228@gmail.com>
2025-03-11 17:10:55 +0200

    Update dz05_03.py

--- dz05_03.py
+++ dz05_03.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import random
+import keyboard
+
 def set_theme(theme):
     global button
     window.config(bg=theme)
@@ -39,8 +41,29 @@ def set_theme(theme):
         display.config(bg='mediumorchid3', fg='white')
         for button in buttons:
             button.config(bg="mediumorchid3", fg="white")
+
 def on_button_click(button):
-    print(button)
+    if button == "=":
+        try:
+            res=eval(display.get())
+            try:
+                res = int(res)
+            except TypeError:
+                1
+        except ZeroDivisionError:
+            display.insert(END, "Error")
+        except Exception as e:
+            print(e)
+            pass
+        else:
+            if display.get()[-1]!="=":
+                display.insert(END, "=")
+            display.insert(END, res)
+    elif button == "C":
+        display.delete(0, END)
+    else:
+        display.insert(END, button)
+
 window = Tk()
 window.title("Калькулятор")
 window.geometry("385x360")
@@ -51,10 +74,10 @@ button_texts = [
 '7', '8', '9', '/',
 '4', '5', '6', '*',
 '1', '2', '3', '-',
-'C', '0', '=', '+'
-]
+'C', '0', '=', '+', '(', ')']
 row_val = 1
 col_val = 0
+
 for text in button_texts:
     button = Button(window, text=text, font=('Arial', 18), width=5, height=2, command=lambda text=text: on_button_click(text))
     button.grid(row=row_val, column=col_val)
@@ -79,4 +102,8 @@ theme_menu.add_command(label="Золота", command=lambda: set_theme("gold2"))
 theme_menu.add_command(label="Випадкова", command=lambda: set_theme(random.choice(colorlist)))
 menubar.add_cascade(label="Налаштування", menu=theme_menu)
 window.config(menu=menubar)
+
+keyboard.add_hotkey("=",lambda: on_button_click("="))
+keyboard.add_hotkey("c",lambda: on_button_click("C"))
+
 window.mainloop()
